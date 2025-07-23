fad8eb7b90de2067c01123bc6593f121cfec40b7
kevit-bhakti-kotak <bhakti.kotak@kevit.io>
2025-07-23 18:21:44 +0530

    login page modified

--- login.js
+++ login.js
@@ -1,39 +1,25 @@
 let validUsers = [
-    { email: "admin@gmail.com", password: "admin123" },
-    { email: "krish@gmail.com", password: "krish123" },
-    { email: "bhakti@gmail.com", password: "bhakti123" },
-    { email: "diya@gmail.com", password: "diya123" },
-    { email: "nidhi@gmail.com", password: "nidhi123" }
+    { username: "editor", password: "editor" },
+    { username: "reviewer", password: "reviewer" }
   ];
   
-
 
   document.querySelector("#login-btn").addEventListener('click', function (e) {
     e.preventDefault();
 
-    const enteredEmail = document.getElementById("email").value;
+    const enteredUsername = document.getElementById("username").value;
     const enteredPassword = document.getElementById("password").value;
 
     //to Check if entered credentials match any in the array
-    console.log(enteredEmail);
-    localStorage.setItem("loggedInEmail", enteredEmail);
+    console.log(username);
+    localStorage.setItem("username", enteredUsername);
     
 
     if (validUsers.some(user => 
-      user.email === enteredEmail && user.password === enteredPassword))
+      user.username === enteredUsername && user.password === enteredPassword))
         {
         window.location.href = "./home.html";
         } else {
             document.getElementById("error").style.display = "block";
                 }
   });
-
-    
-   if (document.querySelector("#welcome-msg")) {
-  const email = localStorage.getItem("loggedInEmail");
-
-  if (email) {
-    const username = email.split("@")[0];
-    document.querySelector("#welcome-msg").textContent = `Welcome ${username}`;
-  } 
-}
